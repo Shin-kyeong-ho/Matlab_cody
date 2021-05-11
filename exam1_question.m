43b5ab79cf74ee5d9d1792711a98b4027f46c95a
Shin-kyeongho <kyon5@naver.com>
2021-05-11 23:31:48 +0900

    exam1_6 -ing

--- exam1_question.m
+++ exam1_question.m
@@ -32,3 +32,46 @@ if(n+1<=length(M))
 end
 Q = M
 end
+
+%% 4번 다항식 보간, 미분, 근 구하기
+
+function c = MiniMaxParabola( p1, p2, p3)
+p=[p1' p2' p3']
+po  = polyfit(p(1,:),p(2,:),2) %2차함수로 보간
+pd = polyder(po); %po미분
+c=roots(pd) %미분된po의 근구하기
+end
+
+
+%% 5번 순환시프트
+
+
+%% 6번 합의 반절되는 원소 찾기 -ing
+
+x = [1 6 4 2 5 7 3];
+x = [2 4 6];
+0 0 0 0
+sum(x)
+j=1;
+%1개가 반값
+if x(i)==sum(x)/2
+    b(j)=i;
+end
+
+%2개 더해서 반값
+if a(i)+a(i+1)==sum(x)/2
+
+%3개 더해서 반값?
+n=3
+for i = 1 : n
+    for j = i+1 : n
+
+
+
+
+
+
+
+
+
+
