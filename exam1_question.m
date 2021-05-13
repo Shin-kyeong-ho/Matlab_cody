d4b2950cd7e659216552e3f2c97e26bb9f49d12e
Shin-kyeongho <kyon5@naver.com>
2021-05-13 23:03:22 +0900

    exam2_start

--- exam1_question.m
+++ exam1_question.m
@@ -1,4 +1,4 @@
-%% 1회 경진대회
+%% 1회 경진대회 기출 문제
 %1번 소수 싫어하는 마방진
 function s =  magic_hates_primes(n)
 a = magic(n);
@@ -141,5 +141,122 @@ for j = 1 : length(x)
 end
 
 end
+
+%% 10번 숫자 만큼 반복되는 벡터 구하기
+
+function x = RepeatNumber(n)
+b=[]
+for n = 1 : n
+    a = n*(ones(1,n))
+    b=[b a]
+end
+x=b
+end
+
+%% 11번 뱀 모양 찾기
+
+function tf = isItSnaky(M)
+[m,n] = size(M)
+t=0;
+for i = 1 : m
+    for j = 1 : n
+        if M(i,j) == 1
+            t = t+1
+        else
+            if i-1 ~= 0 % 상
+                if M(i,j)-M(i-1,j) == 1
+                    t = t+1
+                end
+            end
+            if i+1 ~= m+1 %  하
+                if M(i,j)-M(i+1,j) == 1
+                    t = t+1
+                end
+            end
+            if j-1 ~= 0 % 좌
+                if M(i,j)-M(i,j-1) == 1
+                    t = t+1
+                end
+            end
+            if j+1 ~= n+1 % 우
+                if M(i,j)-M(i,j+1) == 1
+                    t = t+1
+                end
+            end
+        end
+        if t == 0
+            tf = false
+            break
+        else
+            t = 0
+            tf = true
+        end
+    end
+    if tf == false
+        break
+    end
+end
+tf
+end
+
+%% 12번 등비수열 아닌 원소찾기
+
+function [p, w] = GSeqCorrect(v)
+for i = 1 : length(v)-1 %원소 사이간 비율
+    b(i) = v(i+1)/v(i)
+end
+gongbi = mode(b) % 최빈값(=공비) 구하기
+error_b = find(b~=gongbi) %공비가 이상한 부분찾기
+if b(1) == gongbi % 초항은 잘못된 값이 아님
+    w(1) = v(1)
+    for i = 2 : length(v)
+        w(i) = gongbi*w(i-1)
+    end
+    if length(error_b) == 2
+        p = error_b(2)
+    else
+        p = length(v)
+    end
+else %b(1)이 잘못된 값이면 첫번째와 두번쨰 중문제있고, 세번째항은 멀쩡한값이므로
+    w(1) = v(3)/(gongbi^2)
+    for i = 2: length(v)
+        w(i) = gongbi*w(i-1)
+    end
+    if v(3)/gongbi==v(2)
+        p=1;
+    else
+        p=2;
+    end
+end
+end
+
+
+%% 13번 숫자 열을 십진수로 바꾸기
+
+function d= Digits2Number(x)
+n = length(x);
+sum = 0;
+j = 0;
+for i = n : -1 : 1
+    j = j+1;
+    sum = sum + x(j)*10^(i-1)
+end
+d = sum
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ exam2_question.m
@@ -0,0 +1,5 @@
+%% 2회 경진대회 기출 문제
+%1번 암호 해독
+clc; clear;
+C = 'abcdefg  '
+unicodeValues = double(C)
