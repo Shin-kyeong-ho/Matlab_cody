c41771af1c211ee54cf521b810f1d2639d39d9a1
Shin-kyeongho <kyon5@naver.com>
2021-05-14 17:24:54 +0900

    solved_exam2_1

--- exam2_question.m
+++ exam2_question.m
@@ -1,19 +1,57 @@
 %% 2회 경진대회 기출 문제
 %1번 암호 해독
-clc; clear;
-C = 'az'
-k=1
-plus = mod(k,26)
-u = double(C) + k
-char(u)
+function m = decryptMsg( s, p )
+split_s = split(s)
+for i = 1 : length(split_s) 
+if length(char(split_s(i))) == length(p) %같은 길이 찾기
+    disp('같은 길이 찾았다')
+    i
+    for k = 0 : 25
+        a = double(char(split_s(i))) + k;
+        if length(find(a>122)>=1)
+        big = a(find(a>122));
+        a(find(a>122))=mod(big,26)+78; % 97보다 클때
+        end
+        if length(find(a<97)) >= 1
+        small = a(find(a<97));
+        a(find(a<97))=mod(small,26)+104; % 97보다 작을 때
+        end
+        if char(a) == p % 같은 글자 인가
+            disp('같은 글자 찾았다')
+            k
+            break
+        end
+    end
+    if char(a) == p
+        break
+    end
+end
+end
+
+b = double(char(split_s(1)))+k;
+if length(find(b>122)>=1)
+    big = b(find(b>122));
+    b(find(b>122))=mod(big,26)+78; % 97보다 클때
+end
+if length(find(b<97)) >= 1
+    small = b(find(b<97));
+    b(find(b<97))=mod(small,26)+104; % 97보다 작을 때
+end
+c=char(b)
+for i = 2 : length(split_s) 
+    b = double(char(split_s(i)))+k;
+if length(find(b>122)>=1)
+    big = b(find(b>122));
+    b(find(b>122))=mod(big,26)+78; % 97보다 클때
+end
+if length(find(b<97)) >= 1
+    small = b(find(b<97));
+    b(find(b<97))=mod(small,26)+104; % 97보다 작을 때
+end
+c=[c ' ' char(b)]
+m=c
+end
+end
 
 
-plus = mod(k,26)
-
-a = [200 123 148 11]
-
-small = a(find(a<97))
-big = a(find(a>97))
-
-a(find(a>97))=mod(big,26)+78 % 97보다 클때
-a(find(a<97))=mod(small,26)+104 % 97보다 작을 때
+%%
