1bdb96b3c45d019f655ad398639ebb6bd8a85b34
Shin-kyeongho <kyon5@naver.com>
2021-05-15 18:24:29 +0900

    finish

--- practice_2021.m
+++ practice_2021.m
@@ -29,6 +29,59 @@ imshow(G);
 
 %% 2번 정사각형 네 개 그리기
 
+close all;
+x = [-1 1 1 -1 -1];
+y = [-1 -1 1 1 -1];
+
+plot(x,y,'LineWidth',2)
+hold on
+t=(1/4)*pi;
+c = cos(t); s = sin(t);
+R = [c -s ; 
+     s c ];
+p = [x;y]
+p = sqrt(2).*R*[p(1,:);p(2,:)];
+plot(p(1,:),p(2,:),'LineWidth',2)
+p = sqrt(2).*R*[p(1,:);p(2,:)];
+plot(p(1,:),p(2,:),'LineWidth',2)
+p = sqrt(2).*R*[p(1,:);p(2,:)];
+plot(p(1,:),p(2,:),'LineWidth',2)
+grid on
+axis equal
+hold off
+
+%% 3번 행렬에서 숫자 사이의 거리
+
+function d = manhattan( M, a, b )
+    % 아래 코드를 수정하여 문제를 해결하시오.
+[x,y,z] = size(M)
+for i = 1 : z
+    if length(find(M(:,:,i)==a)) == 1 
+        [m1,n1] = find(M(:,:,i)==a)
+        i1 = i
+        break
+    end
+end
+for i = 1 : z
+    if length(find(M(:,:,i)==b)) == 1 
+        [m2,n2] = find(M(:,:,i)==b)
+        i2 = i
+        break
+    end
+end
+
+d=abs(m1-m2)+abs(n1-n2)+abs(i1-i2)
+end
+
+
+
+
+
+
+
+
+
+
 
 
 
