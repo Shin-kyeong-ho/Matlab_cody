a2b8eda22b12ddb0759378aff243bcb1ffe6c366
Shin-kyeongho <kyon5@naver.com>
2021-05-17 10:46:57 +0900

    약수의 개수

--- matlab_grader_mid.m
+++ matlab_grader_mid.m
@@ -22,11 +22,31 @@ s = gcd(n,m)==1
 end
 
 
+%% 제곱수는 몇 개?
 
+function k = countSquared( n )
+a = sqrt(n)
+k=length(find((a-floor(a))==0))
+end
 
+%% 약수의 개수
 
-
-
+function n = countDivisors( x )
+c=[1 x]
+i=1
+while i<c(end)
+i=i+1
+j=i-1
+while i*j<x
+    j=j+1
+    if i*j==x
+        c=[c i j]
+    end
+end
+end
+yaksu=unique(c)
+n=length(yaksu)
+end
 
 
 
