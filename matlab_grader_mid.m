1a8784e8508f359e7ea442745e9a4fa6cec2a0c5
Shin-kyeongho <kyon5@naver.com>
2021-05-18 10:50:24 +0900

    피보나치구하기

--- matlab_grader_mid.m
+++ matlab_grader_mid.m
@@ -49,6 +49,7 @@ n=length(yaksu)
 end
 
 %% 컬러 사각형 만들기
+
 close all; clc; clear;
 w = 100
 rgb =[255 0 0]
@@ -59,11 +60,72 @@ C(:,:,2)=rgb(2)
 C(:,:,3)=rgb(3)
 imshow(C)
 
-%%
+%% 중간고사 2018S 기출문제
+%사각형의 둘레 구하기
 
+function y = rectCircumference(r)
+t2 = pi/2
+a = [r*cos(t2) r*sin(t2)]
+b = [r*cos(t2-(2/3)*pi) r*sin(t2-(2/3)*pi)]
+c = norm(a-b)
+y = 2*c+2*r
+end
 
+%% 행렬 만들기
 
+a=fibonacci(1:12)
+b=reshape(a,[4,3])
+c=zeros(7,7)
+c(1:4,1:3)=b
+c(5:end,4:end)=b'
+M=c
 
+%% 네모 이미지 만들기
+
+W = 200; 
+P = zeros(W,W,3,'uint8');
+P(51:150,51:150,1)=255
+P(51:150,51:150,2)=255
+imshow(P);
+
+%% 그래픽 그리기
+
+x = linspace(0, 1, 100)';
+y1 =3 - cos(2*pi*x)
+y2 =1 + cos(2*pi*x)
+y3 =2 +(1-cos(2*pi*x)).*cos(20*pi*x)
+h = plot(x, y1,x,y2,x,y3); grid on;
+h(1).Color='red'
+h(1).LineStyle=':'
+h(2).Color='blue'
+h(2).LineStyle=':'
+h(3).Color='black'
+h(3).LineStyle='-'
+
+%% 마방진 블럭 이미지
+
+N = 11;
+M = magic(N);
+a = uint8(ones(10));
+G = kron(uint8(M),a);
+imshow(G);
+
+%% 중간고사 2019S 예상문제
+% n번째 소수인 피보나치 구하기
+function f = primeFibo( n )
+i=1
+k=0
+while true
+i=i+1
+fibo=fibonacci(1:i)
+if isprime(fibo(end))==1
+    k = k+1
+    if k == n
+        f = fibo(end)
+        break
+    end
+end
+end
 
 
 
