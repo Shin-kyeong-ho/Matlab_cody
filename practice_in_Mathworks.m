dd5c0bb44636fc0bb033fb5b1b72b9bdd3dad856
Shin-kyeongho <kyon5@naver.com>
2021-05-24 21:07:24 +0900

    5/24 매트랩코디 연습

--- practice_in_Mathworks.m
+++ practice_in_Mathworks.m
@@ -50,14 +50,49 @@ end
 b=find(b==max(b))
 
 
+%% Problem 25. Remove any row in which a NaN appears
+
+ A = [  1   5   8 
+       -3 NaN  14 
+        0   6 NaN;1 3 NaN ];
+ B = [];
+ [m,n] = size(A)
+ for i = 1 : m
+     if sum(A(i,:))>=0 || sum(A(i,:))<0
+         B =[B; A(i,:)]
+     end
+ end
+ 
+ 
+
+%% Problem 1035. Generate a vector like 1,2,2,3,3,3,4,4,4,4
+
+n = 3
+
+a=[];
+for i = 1 : n
+    for j = 1 : i
+        a=[a i]
+    end
+end
+
+%% Problem 15. Find the longest sequence of 1's in a binary sequence.
 
 
+ s = '011110010000000100010111' 
 
-
-
-
-
-
+T=0;
+A=[];
+ for i = 1 : length(s)
+     if s(i) == '1'
+         T=T+1
+         A=[A T];
+     else
+         T=0
+         A=[A T];
+     end
+ end
+ y=max(T)
 
 
 
